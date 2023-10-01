ce6a2ffcc78e448018b6fd1715f0111e87474531
khushpreetsinghb <khushpreets81b@gmail.com>
2023-10-01 17:28:44 +0530

    fixed know our team link in footer

--- index.js
+++ index.js
@@ -1,34 +1,28 @@
 const hamNav = document.getElementById('hamburger_navbar');
 const nav = document.getElementById('nav');
-const contentNav = document.getElementById('navbarSupportedContent');
-const items = document.querySelectorAll('.nav-item');
-let i = 0;
+
 console.log(nav);
 
 hamNav.addEventListener('click', (e) => {
-  if (i%2==0) {
-    nav.style.height = "auto";
-    for (var x = 0; x < items.length; x++){
-      items[x].style.display = 'block';}
-  }
-  else{
-    nav.style.height = "4.3rem";
-    for (var x = 0; x < items.length; x++){
-      items[x].style.display = 'none';}
-  }
-  i++;
-  console.log(i);
+    nav.style.height = "12.3rem";
 })
-
-const knowOurTeamLink = document.getElementById("know-our-team-link");
-const myteamSection = document.getElementById("myteam-section");
-knowOurTeamLink.addEventListener("click", function (event) {
-  event.preventDefault(); // Prevent the default jump behavior
-  // Calculate the offset of the target section from the top of the page
-  const offsetTop = myteamSection.offsetTop;
-  // Scroll to the target section with a smooth animation
+const myTeamSection = document.getElementById("myteam-section");
+function scrollToMyTeamSection() {
+  const offsetTop = myTeamSection.offsetTop;
   window.scrollTo({
     top: offsetTop,
     behavior: "smooth",
   });
+}
+
+const knowOurTeamLink = document.getElementById("know-our-team-link");
+knowOurTeamLink.addEventListener("click", function (event) {
+  event.preventDefault();
+  scrollToMyTeamSection();
+});
+
+const knowOurTeamLinkFooter = document.getElementById("know-our-team-link-footer");
+knowOurTeamLinkFooter.addEventListener("click", function (event) {
+  event.preventDefault();
+  scrollToMyTeamSection();
 });
