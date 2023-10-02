6d88b6f39c04ca4e9967b4f485f92022ccf6f6f6
probot-codes <134484422+probot-codes@users.noreply.github.com>
2023-10-02 17:54:00 +0530

    Event Notification functionality added

--- index.js
+++ index.js
@@ -26,3 +26,89 @@ knowOurTeamLinkFooter.addEventListener("click", function (event) {
   event.preventDefault();
   scrollToMyTeamSection();
 });
+
+function showNotification(eventName) {
+  const notificationContainer = document.getElementById('notification-container');
+  const notification = document.createElement('div');
+  notification.className = 'notification';
+
+  const text = document.createElement('div');
+  text.className = 'notification-text';
+  text.textContent = eventName;
+  
+  notification.classList.add('hidden');
+  notification.appendChild(text);
+  notificationContainer.appendChild(notification);
+  void notification.offsetWidth;
+
+  setTimeout(() => {
+      notification.classList.add('hidden');
+      notification.style.opacity = '0';
+      setTimeout(() => {
+          notificationContainer.removeChild(notification);
+          
+          if (notificationContainer.children.length === 0) {
+              notificationContainer.style.display = 'none';
+          }
+      }, 500);
+  }, 3000);
+
+  notificationContainer.style.display = 'block';
+}
+
+if ("Notification" in window) {
+  Notification.requestPermission()
+    .then((permission) => {
+      if (permission === "granted") {
+        console.log("Notification permission granted.");
+      } else if (permission === "denied") {
+        console.log("Notification permission denied.");
+      } else {
+        console.log("Notification permission not set.");
+      }
+    })
+    .catch((error) => {
+      console.error("Error requesting notification permission:", error);
+    });
+} else {
+  console.log("Notifications not supported in this browser.");
+}
+
+
+function checkEventsForNotifications() {
+  const currentDate = new Date();
+  for (var i = 0; i < localStorage.length; i++) {
+      var eventId = localStorage.key(i);
+      var eventDataJSON = localStorage.getItem(eventId);
+
+      if (eventDataJSON){
+          var eventData = JSON.parse(eventDataJSON);
+          var eventName = eventData.eventName;
+          var eventDate = new Date(eventData.eventDate)
+          var daysRemaining = Math.ceil((eventDate - currentDate) / (1000 * 60 * 60 * 24));
+          if (daysRemaining >= 0) {
+              showNotification(`Event: ${eventName}, ${daysRemaining} days Remaining`);
+          } else {
+              localStorage.removeItem(eventId);
+          }
+      }
+      
+  }
+  const notificationContainer = document.getElementById('notification-container');
+  const reminderText = document.querySelector('.notification-reminder');
+  if (notificationContainer.children.length > 1) {
+      reminderText.style.display = 'block';
+      setTimeout(() => {
+        reminderText.style.display = 'none';
+        notificationContainer.style.display = 'none';
+      }, 3500);
+  } else {
+      reminderText.style.display = 'none';
+  }
+
+  if (notificationContainer.children.length === 1) {
+      reminderText.style.display = 'none';
+  }
+}
+
+window.addEventListener('load', checkEventsForNotifications);
--- past-events.js
+++ past-events.js
@@ -57,3 +57,74 @@ const pastEvents = [
   
   filterEvents("All");
   
+  function subscribeToEvent(eventId) {
+    const eventElement = document.getElementById(eventId);
+    const eventName = eventElement.getAttribute('data-event-name');
+    const eventDate = eventElement.getAttribute('data-event-date');
+    
+    if (!isSubscribed(eventId)) {
+        var eventData = {
+            eventName,
+            eventDate,
+        };
+        var eventDataJSON = JSON.stringify(eventData);
+        console.log(eventDataJSON);
+        localStorage.setItem(eventId, eventDataJSON);
+        toggleButtons(eventId);
+        alert(`You have successfully subscribed to ${eventName}.`);
+    } else {
+        alert(`You are already subscribed to ${eventName}.`);
+    }
+}
+
+function unsubscribeFromEvent(eventId) {
+    const eventElement = document.getElementById(eventId);
+    const eventName = eventElement.getAttribute('data-event-name');
+    if (isSubscribed(eventId)) {
+        localStorage.removeItem(eventId);
+        toggleButtons(eventId);
+        alert(`You have successfully unsubscribed from ${eventName}.`);
+    } else {
+        alert(`You are not subscribed to ${eventName}.`);
+    }
+} 
+
+function isSubscribed(eventName) {
+    return localStorage.getItem(eventName) !== null;
+}
+
+function toggleButtons(eventId) {
+
+    const eventElement = document.getElementById(eventId);
+    const subscribeButton = eventElement.querySelector('.subscribe-button');
+    const unsubscribeButton = eventElement.querySelector('.unsubscribe-button');
+
+    if (localStorage.getItem(eventId)) {
+        subscribeButton.style.display = "none";
+        unsubscribeButton.style.display = "block";
+    } else {
+        subscribeButton.style.display = "block";
+        unsubscribeButton.style.display = "none";
+    }
+}
+
+function updateButtons() {
+    const events = document.querySelectorAll('.event');
+    events.forEach((event) => {
+        const eventId = event.id;
+        const subscribeButton = event.querySelector('.subscribe-button');
+        const unsubscribeButton = event.querySelector('.unsubscribe-button');
+        
+        if (subscribeButton && unsubscribeButton) {
+            if (isSubscribed(eventId)) {
+                subscribeButton.style.display = 'none';
+                unsubscribeButton.style.display = 'block';
+            } else {
+                subscribeButton.style.display = 'block';
+                unsubscribeButton.style.display = 'none';
+            }
+        }
+    });
+}
+
+window.addEventListener('load', updateButtons);
